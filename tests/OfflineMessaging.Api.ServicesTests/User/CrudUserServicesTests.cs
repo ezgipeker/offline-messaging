7c0443069864a74f779d9305a88eba81a2a4adb3
Ezgi Peker <ezgi.peker.6@gmail.com>
2020-03-30 18:35:26 +0300

    CrudUserServicesTests added for AddUserAsync.

---
+++ tests/OfflineMessaging.Api.ServicesTests/User/CrudUserServicesTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using OfflineMessaging.Api.Services.User;
+using OfflineMessaging.Api.ServicesTests.Base.Helpers;
+using OfflineMessaging.Domain.Dtos.User;
+using OfflineMessaging.Infrastructure.Context;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OfflineMessaging.Api.ServicesTests.User
+{
+    public class CrudUserServicesTests
+    {
+        private readonly ICrudUserServices _crudUserServices;
+        public CrudUserServicesTests()
+        {
+            var context = TestHelper.GetInMemoryDbContext<OfflineMessagingContext>();
+            _crudUserServices = new CrudUserServices(context);
+        }
+
+        [Theory]
+        [InlineData("username", "FirstName", "LastName", "email@test.com", "niBbRshzNi1q2F4FOwOq6wfWjvLHJSyJ974Hb8HMY34=ælBZAqjrEVpEmyvVsvzyOtA==")]
+        public async Task AddUserAsync_ShouldReturnSuccessTrue_WhenParametersValid(string userName, string firstName, string lastName, string email, string password)
+        {
+            //Arrange
+            var fixture = new Fixture();
+            var @object = fixture.Build<UserDto>()
+                .With(x => x.UserName, userName)
+                .With(x => x.FirstName, firstName)
+                .With(x => x.LastName, lastName)
+                .With(x => x.Email, email)
+                .With(x => x.Password, password)
+                .Create();
+
+            //Act
+            var sut = await _crudUserServices.AddUserAsync(@object);
+
+            //Assert
+            sut.Should().BeTrue();
+        }
+    }
+}
